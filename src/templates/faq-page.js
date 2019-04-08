d43af6bcab78ff240fc84caa21cee4d0a85ca8e9
vanwap <info@vancitywebandphoto.com>
2019-04-08 15:03:22 -0700

    faq test work 5

--- src/templates/faq-page.js
+++ src/templates/faq-page.js
@@ -2,12 +2,15 @@ import React from 'react'
 import PropTypes from 'prop-types'
 import { graphql } from 'gatsby'
 import Layout from '../components/Layout'
+import Features2 from '../components/Features2'
 import Content, { HTMLContent } from '../components/Content'
 
-export const FaqPageTemplate = ({ title, content, contentComponent, qaContainer }) => {
-  const PageContent = contentComponent || Content
-
-  return (
+export const FaqPageTemplate = ({
+  title,
+  content,
+  contentComponent,
+  qaContainer,
+}) => (
     <section className="section section--gradient">
       <div className="container">
         <div className="columns">
@@ -16,15 +19,13 @@ export const FaqPageTemplate = ({ title, content, contentComponent, qaContainer 
               <h2 className="title is-size-3 has-text-weight-bold is-bold-light">
                 {title}
               </h2>
-              <PageContent className="content" content={content} />
-              <PageContent className="qa" content={qaContainer} />
+              <Features2 gridItems={qaContainer.qa} />
             </div>
           </div>
         </div>
       </div>
     </section>
   )
-}
 
 FaqPageTemplate.propTypes = {
   title: PropTypes.string.isRequired,
@@ -36,32 +37,41 @@ FaqPageTemplate.propTypes = {
 }
 
 const FaqPage = ({ data }) => {
-  const { markdownRemark: post } = data
+  const { frontmatter } = data.markdownRemark
 
   return (
     <Layout>
       <FaqPageTemplate
         contentComponent={HTMLContent}
-        title={post.frontmatter.title}
-        content={post.html}
-        qaContainer={post.frontmatter.qaContainer}
+        title={frontmatter.title}
+        content={frontmatter.html}
+        qaContainer={frontmatter.qaContainer}
       />
     </Layout>
   )
 }
 
 FaqPage.propTypes = {
-  data: PropTypes.object.isRequired,
+  data: PropTypes.shape({
+    markdownRemark: PropTypes.shape({
+      frontmatter: PropTypes.object,
+    }),
+  }),
 }
 
 export default FaqPage
 
 export const FaqPageQuery = graphql`
-  query FaqPage($id: String!) {
-    markdownRemark(id: { eq: $id }) {
-      html
+  query FaqPageTemplate {
+    markdownRemark(frontmatter: { templateKey: { eq: "faq-page" } }) {
       frontmatter {
         title
+        qaContainer {
+          qa {
+            question
+            answer
+          }
+        }
       }
     }
   }
